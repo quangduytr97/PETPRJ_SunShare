beb0d7e3cf9f9986cc6d2aa7d5665ce4bb2dca4c
quangduytr <quangduytr97@gmail.com>
2022-12-11 22:43:53 +0700

    Add new BE app

---
+++ SunShare_BE/src/main/java/com/sunshare/SunShare_BE/SunShareBeApplication.java
@@ -0,0 +1,13 @@
+package com.sunshare.SunShare_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunShareBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunShareBeApplication.class, args);
+	}
+
+}
---
+++ SunShare_BE/src/test/java/com/sunshare/SunShare_BE/SunShareBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunshare.SunShare_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunShareBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
